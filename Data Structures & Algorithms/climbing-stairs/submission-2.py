e33ce8f47a85abd6119972ef059944358d723b60
mansurahmedn286-wq <mansurahmedn286@gmail.com>
2026-06-24 16:18:52 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,13 @@
+from functools import cache
+
+
+class Solution:
+    @cache
+    def climbStairs(self, n: int) -> int:
+        if n==1:
+            return 1
+        elif n==2:
+            return 2    
+        elif n>2:
+            return self.climbStairs(n-1) + self.climbStairs(n-2)
+        return 0
